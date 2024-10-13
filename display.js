a5b2a64cec8f1fef67b3c1517db4949bcd1cd59b
kabeleshh <kabelesh2003@gmail.com>
2024-10-13 16:44:15 +0530

    feat:code modified

--- display.js
+++ display.js
@@ -3,17 +3,21 @@ function display(){
 
     let date = new Date();
 
-    let hour = twodig(date.getHours());
+    let hour = date.getHours();
     let min = twodig(date.getMinutes());
     let sec = twodig(date.getSeconds());
-    
-    document.getElementById("hour").innerHTML = hour;
-    document.getElementById("min").innerHTML = min;
-    document.getElementById("second").innerHTML = sec;
 
     if(hour>=12){
         document.getElementById("meridiam").innerHTML = "PM";
     }
+
+    if(hour>12){
+        hour = twodig(hour - 12);
+    }
+    
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("min").innerHTML = min;    
+    document.getElementById("second").innerHTML = sec;
 }
 
 setInterval(display,1000)
